0ff610bd26b12d47e2cd22ec3bacd3b61d23b1b7
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-03-31 11:54:55 -0300

    02.05_Utilizando_List_Builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,123 +6,74 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-          GestureDetector(
-            onTap: () {},
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Hero(
-                    tag: pets[0].id,
-                    child: Container(
-                      width: double.infinity,
-                      height: 250,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20)
-                        ),
-                        image: DecorationImage(
-                          image: AssetImage(pets[0].imageUrl),
-                          fit: BoxFit.cover
-                        )
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(12, 12, 40, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          pets[0].nome,
-                          style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(12, 12, 40, 0),
-                    child: Text(
-                      pets[0].descricao,
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16,
-                            color: Colors.grey
-                        ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Hero(
-                    tag: pets[1].id,
-                    child: Container(
-                      width: double.infinity,
-                      height: 250,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(20)
-                          ),
-                          image: DecorationImage(
-                              image: AssetImage(pets[1].imageUrl),
-                              fit: BoxFit.cover
-                          )
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(12, 12, 40, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          pets[1].nome,
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(12, 12, 40, 0),
-                    child: Text(
-                      pets[1].descricao,
-                      style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 16,
-                          color: Colors.grey
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: pets.length,
+        itemBuilder: (context, index) {
+          return _petCard(context, index);
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {},
           label: Text("Cadastrar"),
           icon: Icon(Icons.pets),
           backgroundColor: Colors.redAccent,
+      ),
+    );
+  }
+
+  Widget _petCard(BuildContext context, int index) {
+    return GestureDetector(
+      onTap: () {},
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Hero(
+              tag: pets[index].id,
+              child: Container(
+                width: double.infinity,
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(20)
+                    ),
+                    image: DecorationImage(
+                        image: AssetImage(pets[index].imageUrl),
+                        fit: BoxFit.cover
+                    )
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(12, 12, 40, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    pets[index].nome,
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(12, 12, 40, 0),
+              child: Text(
+                pets[index].descricao,
+                style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 16,
+                    color: Colors.grey
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
