daf3556d1a5bda64ecc4210ee29d54e6bf1d5d26
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-04-09 10:11:10 -0300

    06.02_Criando_Card_Remedio

--- lib/screens/pet/remedio_screen.dart
+++ lib/screens/pet/remedio_screen.dart
@@ -52,7 +52,37 @@ class RemedioScreen extends StatelessWidget {
                 ],
               ),
             ),
-
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  Card(
+                    elevation: 8.0,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    child: Container(
+                      child: ListTile(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        leading: Container(
+                          padding: EdgeInsets.only(right: 12),
+                          decoration: BoxDecoration(
+                            border: Border(
+                                right: BorderSide(width: 1.0, color: Colors.redAccent)
+                            ),
+                          ),
+                          child: Icon(Icons.healing, color: Colors.redAccent,),
+                        ),
+                        title: Text(
+                          pet.nome,
+                          style: TextStyle(fontWeight: FontWeight.w400),
+                        ),
+                        subtitle: Text(
+                          pet.descricao,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
 
